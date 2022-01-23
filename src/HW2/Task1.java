da8ddd29f0bdb72b4e929648ae4130076dc48edf
Ole4ka0302 <98165277+Ole4ka0302@users.noreply.github.com>
2022-01-23 17:36:15 +0300

    Add files via upload

---
+++ src/HW2/Task1.java
@@ -0,0 +1,8 @@
+public class Dekart {
+    public static void main(String[] args) {
+        double x1 = 2, y1 = 5, x2 = 5, y2 = 7, x3 = -6, y3 = -2;
+        double z = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1);
+        double sq = Math.abs(z) / 2;
+        System.out.println(sq);
+    }
+}
---
+++ src/HW2/Task2.java
@@ -0,0 +1,10 @@
+public class EvenNumber {
+    public static void main(String[] args) {
+        int number = 45;
+        if (number % 2 == 0)
+            System.out.println("Even Number");
+        else System.out.println("Odd Number");
+
+
+    }
+}
