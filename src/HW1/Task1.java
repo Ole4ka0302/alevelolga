d464f35a8d6a9e3ef45adcf3c8a14d0dd6e3e369
Ole4ka0302 <98165277+Ole4ka0302@users.noreply.github.com>
2022-01-23 17:32:43 +0300

    Add files via upload

---
+++ src/HW1/Task1.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Olga";
+        String surname = "Gostieva";
+        String info = name + " " + surname;
+        System.out.println(info);
+        int length = name.length();
+        if (length > 7) {
+            System.out.println("more than 7");
+        } else {
+            System.out.println("less than 7");
+        }
+    }
+}
